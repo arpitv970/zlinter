06c1a788769e3b887d12cb949293e79b11eb70a3
Arpit Verma <arpitv970@gmail.com>
2023-11-05 21:05:53 +0530

    minor fix

--- lib/seo.ts
+++ lib/seo.ts
@@ -1,5 +1,5 @@
 export const SEOData = {
   title: 'Zintlr',
   description: 'From the most detailed B2B database comprising comprehensive company and profile pages.',
-  img: '/public/seo-img.svg'
+  img: '/public/images/seo-img.svg'
 }
